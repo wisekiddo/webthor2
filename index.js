9d7c3faab4ae6cd25014735fb5cf26259cb0a67f
mojave <ronald@wisekiddo.com>
2020-02-22 11:58:26 +0800

    sdfsdfs

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
 
 
 let app = express();
 
-app.use(function(req, res, next) {
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+});
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+});
+
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+});*/
 
 app
   .use(express.static(path.join(__dirname, 'public')))
