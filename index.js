4709b15b5206eeb4e522c62a518748b3f58e9448
mojave <ronald@wisekiddo.com>
2020-02-22 12:19:57 +0800

    corrs

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ const PORT = process.env.PORT || 5000;
 let app = express();
 
 
-app.use(cors());
+/*app.use(cors());
 
 app.get('/products/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
-});
+});*/
 
 /*app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
 });*/
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Origin', "https://webthor2.herokuapp.com");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
