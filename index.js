e4edd2d0e8d9a11097459dc8ea19de1dae07860e
mojave <ronald@wisekiddo.com>
2020-02-22 14:00:25 +0800

    sdfsdf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 let app = express();
 
 
-/*app.use(cors());
+app.use(cors());
 
 app.get('/products/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
-});*/
+});
 
 /*app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
@@ -27,6 +28,9 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+
     next();
 });
 
@@ -36,3 +40,23 @@ app.use(express.static(path.join(__dirname, 'public')))
     .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+
+
+request.get('/torrents')
+    .expect('Access-Control-Allow-Origin', '*')
+    .expect('x-request-url', 'https://webtorrent.io/torrents')
+    .expect(200, 'RESPONSE FROM EXAMPLE.COM', done);
+
+
+app.get('/torrents', (req, res) => {
+    request(
+        { url: 'https://webtorrent.io/torrents' },
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: error.message });
+            }
+
+            res.json(JSON.parse(body));
+        }
+    )
+});
