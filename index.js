6687cdc20522b4fbb74489b48a001e1b7fe1c855
mojave <ronald@wisekiddo.com>
2020-02-22 12:04:21 +0800

    sdfweewr

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.get('/products/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
 });
 
-app.listen(80, function () {
+/*app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
-});
+});*/
 
 /*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
