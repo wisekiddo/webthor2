28a3cd86bec1db19ce8c336aeed316f4767cc7fc
mojave <ronald@wisekiddo.com>
2020-02-22 12:12:57 +0800

    sdfsdf

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 
 let app = express();
 
 
-app.use(cors())
+app.use(cors());
 
 app.get('/products/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
@@ -18,15 +18,18 @@ app.get('/products/:id', function (req, res, next) {
     console.log('CORS-enabled web server listening on port 80')
 });*/
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', "https://webthor2.herokuapp.com");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
 app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
