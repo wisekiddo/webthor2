338fedc1ecb78c85c9532722acb676780cca36f8
mojave <ronald@wisekiddo.com>
2020-02-22 14:21:56 +0800

    sdfsdf

--- index.js
+++ index.js
@@ -40,14 +40,10 @@ app.use(express.static(path.join(__dirname, 'public')))
     .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
