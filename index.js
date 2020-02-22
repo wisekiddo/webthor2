b63a37537e1b9dee97701e43ca3e4c590d984048
mojave <ronald@wisekiddo.com>
2020-02-22 11:35:57 +0800

    sdfsdfertwe

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+
+let app = express();
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(allowCrossDomain)
