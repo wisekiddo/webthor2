e5a9cf05b8ea277e4d2cea09eae3811716f97fe3
mojave <ronald@wisekiddo.com>
2020-02-22 11:40:49 +0800

    Sqerwerwe

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const PORT = process.env.PORT || 5000
 
 
 let app = express();
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "*");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-}
+});
 
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(allowCrossDomain)
