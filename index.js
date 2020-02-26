51ca8e914815d8e1a7bde20e4e110ff8f860d991
mojave <ronald@wisekiddo.com>
2020-02-26 09:31:03 +0800

    kkhgfdd

--- index.js
+++ index.js
@@ -15,31 +15,40 @@ app.use(function(req, res, next) {
     next();
 });
 
-/*app.get('/with-cors', cors(), (req, res, next) => {
-    res.json({ msg: 'WHOAH with CORS it works! 🔝 🎉' })
-})*/
+var WebTorrent = require('webtorrent')
 
-/*app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-});*/
+var client = new WebTorrent()
 
-/*app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-});*/
+var torrentId = 'magnet:?xt=urn:btih:9EFF2A521DD4F44B057A0C0A550C90A8243D3F17&tr=udp://explodie.org:6969&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.empire-js.us:1337&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.opentrackr.org:1337&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz&tr=wss://tracker.openwebtorrent.com&ws=https://webtorrent.io/torrents/&xs=https://webtorrent.io/torrents&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://p4p.arenabg.com:1337&tr=udp://tracker.leechers-paradise.org:6969';
 
-/*app.use(function (req, res, next) {
+client.add(torrentId, function (torrent) {
+    // Torrents can contain many files. Let's use the .mp4 file
+    var file = torrent.files.find(function (file) {
+        return file.name.endsWith('.mp4')
+    });
 
-    //res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Origin', "https://webthor2.herokuapp.com");
-    res.header('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Display the file by adding it to the DOM.
+    // Supports video, audio, image files, and more!
+   // file.appendTo('body')
+
+    torrent.on('download', function (bytes) {
+        console.log('just downloaded: ' + bytes)
+        console.log('total downloaded: ' + torrent.downloaded)
+        console.log('download speed: ' + torrent.downloadSpeed)
+        console.log('progress: ' + torrent.progress)
+        console.log('progress: ' + file.path);
+        console.log('progress: ' + file.name)
+
+       /* file.getBlobURL(function (err, url) {
+            if (err) throw err
+            console.log('url: ' + url)
+        })*/
+    });
 
 
-    next();
-});*/
+
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
